94886916e4524a83f0a2d02fbe82d38f4234347c
Pankaj Wadhwa <pankaj.wadhwa@usbank.com>
2020-04-06 22:39:32 +0530

    Framework creation

---
+++ PWMultiStateSwitch/PWMultiStateSwitch.h
@@ -0,0 +1,19 @@
+//
+//  PWMultiStateSwitch.h
+//  PWMultiStateSwitch
+//
+//  Created by Pankaj Wadhwa on 06/04/20.
+//  Copyright © 2020 Pankaj Wadhwa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PWMultiStateSwitch.
+FOUNDATION_EXPORT double PWMultiStateSwitchVersionNumber;
+
+//! Project version string for PWMultiStateSwitch.
+FOUNDATION_EXPORT const unsigned char PWMultiStateSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PWMultiStateSwitch/PublicHeader.h>
+
+
